0c1f01ca7f6a27f04d59f28ab2f42a8cc3bd9de1
ElCibernetico86 <elcibernetico86@gmail.com>
2026-04-20 04:30:40 -0500

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 import {defineConfig} from 'vite';
 
 export default defineConfig({
-  base: '/WALEX/',
+  base: '/WALLEX/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
